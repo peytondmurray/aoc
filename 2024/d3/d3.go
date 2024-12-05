b02f4de8612f4d58a2de2c1d57bcdc418e3b7ec9
pdmurray <peynmurray@gmail.com>
2024-12-04 18:07:14 -0800

    Day 3

---
+++ 2024/d3/d3.go
@@ -0,0 +1,62 @@
+package d3
+
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func readData() string {
+	result, err := os.ReadFile("d3/input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(result)
+}
+
+// enableDoDont toggles whether the `do()` and `don't()` statements matter
+func extractValues(str string, enableDoDont bool) [][]int {
+	var result [][]int
+
+	enabled := true
+	re := regexp.MustCompile(`(mul\((?P<v1>\d+),(?P<v2>\d+)\)|do\(\)|don't\(\))`)
+	for _, match := range re.FindAllStringSubmatch(str, -1) {
+
+		if match[1] == "don't()" {
+			enabled = false
+		} else if match[1] == "do()" {
+			enabled = true
+		} else {
+			if !enableDoDont || enabled {
+				v1, err1 := strconv.Atoi(match[2])
+				v2, err2 := strconv.Atoi(match[3])
+
+				if err1 != nil || err2 != nil {
+					log.Fatal("Problem converting this match to integers: ", match)
+				}
+
+				result = append(result, []int{v1, v2})
+			}
+		}
+	}
+	return result
+}
+
+func sumPairs(pairs [][]int) int {
+	result := 0
+	for _, pair := range pairs {
+		result += pair[0]*pair[1]
+	}
+	return result
+}
+
+
+func Run() {
+	str := readData()
+
+	fmt.Println("[d2.1] sum of multiplications: ", sumPairs(extractValues(str, false)))
+	fmt.Println("[d2.2] sum of multiplications with do() and don't(): ", sumPairs(extractValues(str, true)))
+}
--- 2024/main.go
+++ 2024/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"aoc2024/d1"
 	"aoc2024/d2"
+	"aoc2024/d3"
 )
 
 func main() {
 	d1.Run()
 	d2.Run()
+	d3.Run()
 }
