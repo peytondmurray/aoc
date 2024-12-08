5ce1caa4b93eae51fd374389f0872149b3580457
pdmurray <peynmurray@gmail.com>
2024-12-07 22:42:39 -0800

    D6P2!

--- 2024/d6/d6.go
+++ 2024/d6/d6.go
@@ -42,56 +42,93 @@ func (a Direction) Turn() Direction {
 	return None
 }
 
+func newState(init [][]rune) State {
+	obstructions := make(map[Location]struct{})
+	var location Location
+	for i, row := range init {
+		for j, char := range string(row) {
+			if char == '#' {
+				obstructions[Location{j, i}] = struct{}{}
+			} else if char == '^' {
+				location = Location{j, i}
+			}
+		}
+	}
+
+	// Initialize the visited location list of the guard;
+	// first movement is up
+	visited := make(map[Location]map[Direction]struct{}, 0)
+	visited[location] = make(map[Direction]struct{})
+	addToSet(visited[location], Up)
+
+	return State{
+		obstructions,
+		visited,
+		location,
+		Up,
+		[]int{len(init), len(init[0])},
+	}
+}
+
+func addToSet[T comparable](set map[T]struct{}, dir T) {
+	set[dir] = struct{}{}
+}
+
+func setContains[T comparable](set map[T]struct{}, dir T) bool {
+	_, contains := set[dir]
+	return contains
+}
+
 type State struct {
-	state [][]rune
+	obstructions map[Location]struct{}
+	visited map[Location]map[Direction]struct{}
 	location Location
 	direction Direction
-	nNewSteps int
+	shape []int
 }
-
-func (a *State) At(loc Location) rune {
-	if a.isInBounds(loc) {
-		return a.state[loc.y][loc.x]
-	}
-	return -1
-}
-
-func (a *State) Set(loc Location, value rune) {
-	a.state[loc.y][loc.x] = value
-}
-
 
 func (a *State) isInBounds(loc Location) bool {
-	return (loc.y >= 0 && loc.y < len(a.state)) && (loc.x >= 0 && loc.x < len(a.state[0]))
+	return (loc.y >= 0 && loc.y < a.shape[0]) && (loc.x >= 0 && loc.x < a.shape[1])
 }
 
-func (a *State) print() {
-	for i, row := range a.state {
-		fmt.Printf("%4d: %v\n", i, string(row))
-	}
-}
-
-func (a *State) Evolve() {
+// Evolve Walk the guard around until
+//
+// 1. They go off the grid (returns false)
+// 2. They go in a loop (returns true)
+func (a *State) Evolve() bool {
 	for {
 		nextLoc := a.location.Add(a.direction)
-		nextRune := a.At(nextLoc)
-		if nextRune == '.' {
-			a.Set(nextLoc, 'X')
-			a.nNewSteps = a.nNewSteps + 1
-			a.location = nextLoc
-		} else if nextRune == '#' {
+		if setContains(a.obstructions, nextLoc) {
 			a.direction = a.direction.Turn()
-		} else if nextRune == 'X' {
-			a.location = nextLoc
-		} else if nextRune == -1 {
-			// We're off the map; return
-			a.Set(a.location, 'X')
-			a.nNewSteps = a.nNewSteps + 1
-			return
+		} else {
+			directions, visited := a.visited[nextLoc]
+			if visited && setContains(directions, a.direction) {
+				// We've visited already because the next step is on
+				// the same path in the same direction as before
+				return true
+			} else if a.isInBounds(nextLoc) {
+				// If we're visiting a site in bounds, add the location
+				// and current direction to the visited locations
+				if a.visited[nextLoc] == nil {
+					a.visited[nextLoc] = make(map[Direction]struct{})
+				}
+				addToSet(a.visited[nextLoc], a.direction)
+				a.location = nextLoc
+			} else {
+				// If we've left the grid, exit
+				return false
+			}
 		}
 	}
 }
 
+func (a *State) getVisited() []Location {
+	var result []Location
+	for loc := range a.visited {
+		result = append(result, loc)
+	}
+	return result
+}
 
 func readData() [][]rune {
 	file, err := os.Open("d6/input")
@@ -110,42 +147,39 @@ func readData() [][]rune {
 	return result
 }
 
-func findGuard(state [][]rune) Location {
-	for i, row := range state {
-		for j, char := range string(row) {
-			if char == '^' {
-				return Location{j, i}
-			}
-		}
-	}
-	return Location{-1, -1}
-}
-
 func evolve(init [][]rune) State {
-
-	location := findGuard(init)
-	if location.x == -1 {
-		log.Fatal("Error finding guard.")
-	}
-
-
-	// Initialize the state of the guard;
-	// first movement is up
-	guardState := State{
-		init,
-		location,
-		Up,
-		0,
-	}
-
-	// Run the simulation
+	guardState := newState(init)
 	guardState.Evolve()
 	return guardState
 }
 
-func Run() {
-	state := evolve(readData())
+func nObstructedLoops(init [][]rune) int {
+	guardState := newState(init)
+	guardState.Evolve()
 
-	fmt.Println("[d6.1] number visited: ", state.nNewSteps)
-	// fmt.Println("[d6.2] sum of middle values of fixed updates: ", sumAllMiddles(fixed))
+	var loopLocs []Location
+
+	visited := guardState.getVisited()
+	for i, loc := range visited {
+		fmt.Print("\033[G\033[K")
+		fmt.Printf("Obstruction position: %5d/%5d\n", i, len(visited))
+		fmt.Print("\033[A")
+
+		obstructedState := newState(init)
+		addToSet(obstructedState.obstructions, loc)
+		if obstructedState.Evolve() {
+			loopLocs = append(loopLocs, loc)
+		}
+	}
+
+	return len(loopLocs)
+}
+
+func Run() {
+	init := readData()
+	state := evolve(init)
+	nLoops := nObstructedLoops(init)
+
+	fmt.Println("[d6.1] number visited: ", len(state.visited))
+	fmt.Println("[d6.2] number of obstructed loops: ", nLoops)
 }
