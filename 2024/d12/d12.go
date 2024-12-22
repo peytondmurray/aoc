c82b39df97ac7f3f2cdc4de73350dfe61860a9ae
pdmurray <peynmurray@gmail.com>
2024-12-22 00:34:33 -0800

    D12P2 up!

--- 2024/d12/d12.go
+++ 2024/d12/d12.go
@@ -45,6 +45,7 @@ type Location struct {
 	y int
 }
 
+// explore Generate a map of locations containing the same letter as and connected to `location`.
 func explore(data [][]string, char string, location Location, visited map[Location]string) map[Location]struct{} {
 	if location.x < 0 || location.x >= len(data[0]) || location.y < 0 || location.y >= len(data) {
 		return make(map[Location]struct{})
@@ -95,6 +96,8 @@ func explore(data [][]string, char string, location Location, visited map[Locati
 	return locations
 }
 
+// analyze Cound the number of fences for a set of locations;
+// used in d12p1.
 func analyze(locations map[Location]struct{}) (int, int) {
 	perimeter := 0
 	for loc := range locations {
@@ -121,6 +124,34 @@ func analyze(locations map[Location]struct{}) (int, int) {
 	return len(locations), perimeter
 }
 
+// printRegion Print a region, including highlighted letters.
+func printRegion(data [][]string, highlight []Location) {
+
+	locs := make(map[Location]struct{})
+	for _, loc := range highlight {
+		locs[loc] = struct{}{}
+	}
+
+	reset := "\033[0m"
+	red := "\033[31m"
+
+	fmt.Print("\n")
+	for i, row := range data {
+		for j, char := range row {
+			if _, exists := locs[Location{j, i}]; exists {
+				fmt.Print(red, char, reset)
+			} else {
+				fmt.Print(char)
+			}
+		}
+		fmt.Print("\n")
+	}
+
+	fmt.Println()
+}
+
+
+// parseRegion Fully explore all connected sites of a region on the grid.
 func parseRegion(data [][]string, location Location, visited map[Location]string) Region {
 	char := data[location.y][location.x]
 	locations := explore(data, char, location, visited)
@@ -129,6 +160,7 @@ func parseRegion(data [][]string, location Location, visited map[Location]string
 	return Region{char, area, perimeter, locations}
 }
 
+// parseRegions Parse the data into regions.
 func parseRegions(data [][]string) []Region {
 	visited := make(map[Location]string)
 	var regions []Region
@@ -145,13 +177,110 @@ func parseRegions(data [][]string) []Region {
 	return regions
 }
 
+// computePrice Compute the price for d12p1.
 func computePrice(regions []Region) int {
 	result := 0
 	for _, region := range regions {
 		pt := region.area*region.perimeter
 		result += pt
+	}
+	return result
+}
 
-		fmt.Println(region.symbol, ":", "area", region.area, "perimeter", region.perimeter, "cost", pt)
+// raster "Raster" across the dataset to find the edges of a region.
+// Use these edges to find the number of sides of each region, which is
+// the perimeter in d12p2.
+func raster(data [][]string, region Region) int {
+
+	// Find the edges of the dataset
+	lEdges := make([][]int, len(data))
+	rEdges := make([][]int, len(data))
+	uEdges := make([][]int, len(data))
+	dEdges := make([][]int, len(data))
+	for i := range data {
+		lEdges[i] = make([]int, len(data[0]))
+		rEdges[i] = make([]int, len(data[0]))
+		uEdges[i] = make([]int, len(data[0]))
+		dEdges[i] = make([]int, len(data[0]))
+	}
+
+	for loc := range region.locations {
+		left := Location{loc.x-1, loc.y}
+		right := Location{loc.x+1, loc.y}
+		up := Location{loc.x, loc.y-1}
+		down := Location{loc.x, loc.y+1}
+
+		if _, exists := region.locations[left]; !exists {
+			lEdges[loc.y][loc.x] = 1
+		}
+
+		if _, exists := region.locations[right]; !exists {
+			rEdges[loc.y][loc.x] = 1
+		}
+
+		if _, exists := region.locations[up]; !exists {
+			uEdges[loc.y][loc.x] = 1
+		}
+
+		if _, exists := region.locations[down]; !exists {
+			dEdges[loc.y][loc.x] = 1
+		}
+	}
+
+	// Looking at the left and right edges, these from top
+	// to bottom; consecutive flips don't count.
+	sum := 0
+	for j := range data[0] {
+		lastL := 0
+		lastR := 0
+		for i := range data {
+			if lastL == 0 && lEdges[i][j] == 1 {
+				sum++
+			}
+
+			if lastR == 0 && rEdges[i][j] == 1 {
+				sum++
+			}
+
+			lastL = lEdges[i][j]
+			lastR = rEdges[i][j]
+		}
+	}
+
+	// Looking at the top and bottom edges, sum left to
+	// right; consecutive flips don't count.
+	for i := range data {
+		lastU := 0
+		lastD := 0
+		for j := range data[0] {
+			if lastU == 0 && uEdges[i][j] == 1 {
+				sum++
+			}
+
+			if lastD == 0 && dEdges[i][j] == 1 {
+				sum++
+			}
+			lastU = uEdges[i][j]
+			lastD = dEdges[i][j]
+		}
+	}
+
+	return sum
+}
+
+// printData Print the dataset.
+func printData[T comparable](data [][]T) {
+	for _, row := range data {
+		fmt.Println(row)
+	}
+	fmt.Println()
+}
+
+// computeAdjustedPrice Compute the price for d12p2.
+func computeAdjustedPrice(regions []Region, data [][]string) int {
+	result := 0
+	for _, region := range regions {
+		result += region.area*raster(data, region)
 	}
 	return result
 }
@@ -161,5 +290,5 @@ func Run() {
 	regions := parseRegions(data)
 
 	fmt.Println("[d12.1] Fence pricing:", computePrice(regions))
-	// fmt.Println("[d11.2] Number of stones after 75 blinks:", lenAfterNBlinks(stones, 75))
+	fmt.Println("[d12.2] Fence pricing with joined edges:", computeAdjustedPrice(regions, data))
 }
