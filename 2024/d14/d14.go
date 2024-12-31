6e4e9bb00867e89a37351c34d17a152850a4e9a0
pdmurray <peynmurray@gmail.com>
2024-12-30 20:26:57 -0800

    D14P2

--- 2024/d14/d14.go
+++ 2024/d14/d14.go
@@ -33,6 +33,7 @@ type Robot struct {
 	vy int
 }
 
+// newRobot Generate a new robot from the input text.
 func newRobot(text string) Robot {
 	re := regexp.MustCompile(`p=(?P<x>\d+),(?P<y>\d+) v=(?P<vx>-?\d+),(?P<vy>-?\d+)`)
 	match := re.FindStringSubmatch(text)
@@ -54,6 +55,7 @@ type Grid struct {
 	yshape int
 }
 
+// evolve Evolve the grid forward in time.
 func (a *Grid) evolve(time int) {
 	for i, robot := range a.robots {
 		newX := (robot.x + time*robot.vx) % a.xshape
@@ -71,6 +73,7 @@ func (a *Grid) evolve(time int) {
 	}
 }
 
+// calculateSafetyFactor Calculate the safety factor for the grid.
 func (a *Grid) calculateSafetyFactor() int {
 	q1, q2, q3, q4 := 0, 0, 0, 0
 
@@ -92,6 +95,8 @@ func (a *Grid) calculateSafetyFactor() int {
 	return q1*q2*q3*q4
 }
 
+// print Print the grid; if quads == true, the middle vertical
+// and horizontal rows are omitted.
 func (a *Grid) print(quads bool) {
 	grid := toIntGrid(a)
 
@@ -116,6 +121,7 @@ func (a *Grid) print(quads bool) {
 	fmt.Print("\n")
 }
 
+// toIntGrid Render the sparse grid as a dense grid of integers.
 func toIntGrid(a *Grid) [][]int {
 	grid := make([][]int, a.yshape)
 	for i := range a.yshape {
@@ -137,6 +143,10 @@ type Region struct {
 	locations map[Location]struct{}
 }
 
+func (a *Region) area() int {
+	return len(a.locations)
+}
+
 // addToSet Add all items to set
 func addToSet[T comparable](set map[T]struct{}, items ...T) {
 	for _, item := range items {
@@ -151,8 +161,8 @@ func joinSets[T comparable](set map[T]struct{}, other map[T]struct{}) {
 	}
 }
 
+// getConnected Greedily search for locations connected to the given location.
 func getConnected(loc Location, robots map[Location]struct{}, visited map[Location]struct{}) map[Location]struct{} {
-
 	// If we've already visited the current location, just return
 	if _, exists := visited[loc]; exists {
 		return make(map[Location]struct{})
@@ -177,6 +187,7 @@ func getConnected(loc Location, robots map[Location]struct{}, visited map[Locati
 	return connected
 }
 
+// findRegions Find all the different regions in the current grid.
 func findRegions(grid *Grid) []Region {
 	var regions []Region
 
@@ -188,26 +199,18 @@ func findRegions(grid *Grid) []Region {
 
 	// Break into regions
 	visited := make(map[Location]struct{})
-	for i := range grid.yshape {
-		for j := range grid.xshape {
-			loc := Location{j, i}
-
-			if _, exists := visited[loc]; !exists {
-				regions = append(regions, Region{getConnected(loc, robots, visited)})
-			}
+	for robot := range robots {
+		if _, exists := visited[robot]; !exists {
+			regions = append(regions, Region{getConnected(robot, robots, visited)})
 		}
 	}
-
 	return regions
 }
 
-func (a *Region) area() int {
-	return len(a.locations)
-}
-
+// maybeChristmas Check if the grid has a grouping of 10 or more robots.
+// Could they be in the shape of a christmas tree?
 func maybeChristmas(grid *Grid) bool {
 	regions := findRegions(grid)
-
 	for _, region := range regions {
 		if region.area() > 10 {
 			return true
@@ -216,34 +219,29 @@ func maybeChristmas(grid *Grid) bool {
 	return false
 }
 
-
+// evolveTilChristmas Evolve the grid until a grouping of robots appears;
+// then print the grid and wait for the user to look to see if it's a
+// christmas tree. Press return to continue evolving.
 func (a *Grid) evolveTilChristmas() int {
-
-	for i := 0;; i++ {
-		fmt.Print("\033[G\033[K")
-		fmt.Printf("%4d", i)
-		fmt.Print("\033[A")
-
+	for i := 1;; i++ {
+		a.evolve(1)
 		if maybeChristmas(a) {
-			fmt.Print("\033[H\033[2J")
 			fmt.Println("i = ", i)
 			a.print(false)
+
+			// Wait for the user to press return before continuing
 			var input string
 			fmt.Scanln(&input)
 			if input != "\n" {
-				return i
+				continue
 			}
 		}
 	}
 }
 
-
 func Run() {
 	grid := readData()
-
-	// grid.evolve(100)
-	// grid.print(true)
-
-	// fmt.Println("[d14.1] safety factor:", grid.calculateSafetyFactor())
+	grid.evolve(100)
+	fmt.Println("[d14.1] safety factor:", grid.calculateSafetyFactor())
 	fmt.Println("[d14.2] evolving to christmas:", grid.evolveTilChristmas())
 }
