6ac30e1c18cc10bd9ee25c3d8b90f7553b5288c4
pdmurray <peynmurray@gmail.com>
2025-02-05 22:32:23 -0800

    D16P1 complete

--- 2024/d16/d16.go
+++ 2024/d16/d16.go
@@ -54,6 +54,11 @@ func (a *Location) step(b Direction) Location {
 	return Location{a.x + b.dx, a.y + b.dy}
 }
 
+type LocDir struct {
+	loc Location
+	dir Direction
+}
+
 
 type Grid struct {
 	arr [][]string
@@ -81,8 +86,8 @@ func newGrid(arr [][]string) Grid {
 }
 
 func readData() Grid {
-	file, err := os.Open("d16/input2")
-	// file, err := os.Open("d16/input")
+	// file, err := os.Open("d16/input2")
+	file, err := os.Open("d16/input")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -103,42 +108,73 @@ func readData() Grid {
 	return newGrid(arr)
 }
 
-func (a *Grid) solve(loc Location, dir Direction, visited map[Location]struct{}) int {
+func (a *Grid) solve(loc Location, dir Direction, visited map[LocDir]int, currentCost int) (int, bool) {
 
 	// Reached the end
-	if loc.x == a.end.x && loc.y == a.end.y {
-		return 0
+	if loc == a.end {
+		return currentCost, true
 	}
 
-	visited[loc] = struct{}{}
+	// If you've come upon the same location and direction,
+	// don't bother continuing if the cost was lower in the other code path
+	state := LocDir{loc, dir}
+	if cachedCost, exists := visited[state]; exists && cachedCost < currentCost {
+		return 0, false
+	}
+
+
+	// Store the cost of getting to the current location
+	visited[state] = currentCost
 
 	var costs []int
-	leftLoc := loc.step(dir.left())
+	leftDir := dir.left()
+	leftLoc := loc.step(leftDir)
 	if a.at(leftLoc) != "#" {
-		if _, exists := visited[leftLoc]; !exists {
-				costs = append(costs, 1000 + a.solve(loc, dir.left(), visited))
+		nextCost := 1000 + currentCost
+		cachedCost, exists := visited[LocDir{loc, leftDir}]
+		if !exists || cachedCost > nextCost {
+			restCost, okay := a.solve(loc, leftDir, visited, nextCost)
+			if okay {
+				costs = append(costs, restCost)
+			}
 		}
 	}
 
-	rightLoc := loc.step(dir.right())
+	rightDir := dir.right()
+	rightLoc := loc.step(rightDir)
 	if a.at(rightLoc) != "#" {
-		if _, exists := visited[rightLoc]; !exists {
-			costs = append(costs, 1000 + a.solve(loc, dir.right(), visited))
+		nextCost := 1000 + currentCost
+		cachedCost, exists := visited[LocDir{loc, rightDir}]
+		if !exists || cachedCost > nextCost {
+			restCost, okay := a.solve(loc, rightDir, visited, nextCost)
+			if okay {
+				costs = append(costs, restCost)
+			}
 		}
 	}
 
 	forwardLoc := loc.step(dir)
 	if a.at(forwardLoc) != "#" {
-		if _, exists := visited[forwardLoc]; !exists {
-			costs = append(costs, 1 + a.solve(loc.step(dir), dir, visited))
+		nextCost := 1 + currentCost
+		cachedCost, exists := visited[LocDir{forwardLoc, dir}]
+		if !exists || cachedCost > nextCost {
+			restCost, okay := a.solve(forwardLoc, dir, visited, nextCost)
+			if okay {
+				costs = append(costs, restCost)
+			}
 		}
 	}
 
-	return slices.Min(costs)
+	if len(costs) == 0 {
+		return 0, false
+	}
+	return slices.Min(costs), true
 }
 
 func Run() {
 	grid := readData()
 
-	fmt.Println("[d16.1] min cost:", grid.solve(grid.start, East, make(map[Location]struct{})))
+	minCost, okay := grid.solve(grid.start, East, make(map[LocDir]int), 0)
+	fmt.Println("[d16.1] min cost:", minCost, okay)
+	fmt.Println("[d16.2] min cost:", minCost, okay)
 }
